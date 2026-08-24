1f7b090d38576660ee4b5bcfafdb507a65e2b3de
Guard Test <guard@test>
2026-08-24 15:03:48 -0500

    base

--- src/components/hand-replayer/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Hand Replayer Components Index
- * Central exports for all hand replayer components
- */
-
-export { default as PositionAnalysis } from './PositionAnalysis';
-export { default as OddsDisplay } from './OddsDisplay';
-export { default as ShareableHighlight } from './ShareableHighlight';
--- src/components/tabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { Tabs } from './Tabs';
-export { TabPanel } from './TabPanel';
-export { SegmentedControl } from './SegmentedControl';
-export { Stepper } from './Stepper';
